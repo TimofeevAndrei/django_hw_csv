201f225802d6acf0ad1751761305dd004897545b
TimofeevAndrei <i.pika4u@gmail.com>
2023-03-29 13:46:00 +0700

    db create

--- phones/models.py
+++ phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
+    name = models.CharField(max_length=50, null=False)
+    price = models.DecimalField(max_digits=12, decimal_places=0, null=False)
+    image = models.CharField(max_length=1000, null=False)
+    release_date = models.CharField(max_length=50, null=False)
+    lte_exists  = models.BooleanField(null=False)
+    slug = models.SlugField(max_length=200, null=False)
     pass
