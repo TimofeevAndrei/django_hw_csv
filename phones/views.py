92484bbc98fa3ac1035c807358c1a3423b6c0fda
TimofeevAndrei <i.pika4u@gmail.com>
2023-03-29 16:29:19 +0700

    catalog add views

--- phones/views.py
+++ phones/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    list_phone = Phone.objects.all()
     template = 'catalog.html'
-    context = {}
+    context = {
+        'phones': list_phone,
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
+    phone = Phone.objects.get(slug)
     template = 'product.html'
-    context = {}
+    context = {'phone': phone}
     return render(request, template, context)
